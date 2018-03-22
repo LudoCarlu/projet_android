fc737402d2aea8da697d165e9b8f36b37c8cf3d0
faivrem <max.faivre6@free.fr>
2018-03-22 17:55:48 +0100

    Implémentation Menu Principale
    - Fonction About Us
    - Ajout bouton recherche
    - Menu latérale (Accueil/Film/Serie)

--- Projet/app/src/main/java/com/example/maxime/hellocine/MainActivity.java
+++ Projet/app/src/main/java/com/example/maxime/hellocine/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.maxime.hellocine;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,10 +39,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.aboutus:
+                new AlertDialog.Builder(MainActivity.this).setMessage(R.string.aboutus_text).show();
+                return true;
+/*            case R.id.item2:
+                Toast.makeText(MainActivity.this, "Toast : You press pause", Toast.LENGTH_SHORT).show();
+                return true;*/
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
 
 }
